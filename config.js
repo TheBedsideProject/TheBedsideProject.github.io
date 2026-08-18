b369656cf9fa8b217bdd406bdd27e7cc615109cb
TheBedsideProject <jhareyansh83@gmail.com>
2026-08-17 22:11:51 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// --- DATABASE CREDENTIALS ---
+const PROJECT_ID = 'gxfkjcsvbwfzwkntsnbz';
+const SUPABASE_URL = 'https' + '://' + PROJECT_ID + '.supabase' + '.co';
+const SUPABASE_KEY = 'sb_publishable_rFYdZiqeDOkZ1yXnkOqB2Q_PgOkcb1m';
+const ROOM_ID = 'room-101'; 
